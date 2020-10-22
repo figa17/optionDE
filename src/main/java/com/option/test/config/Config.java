1964f100df23f18bda7c0ea6d99ce66252dac77f
Felipe González <fel.alfaro@gmail.com>
2020-10-21 23:56:49 -0300

    first commit

---
+++ src/main/java/com/option/test/config/Config.java
@@ -0,0 +1,12 @@
+package com.option.test.config;
+
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.core.publisher.PubSubPublisherTemplate;
+import org.springframework.cloud.gcp.pubsub.core.subscriber.PubSubSubscriberTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+}
---
+++ src/main/java/com/option/test/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.option.test.controllers;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "Index")
+public class IndexController {
+
+
+    @GetMapping()
+    public ResponseEntity<String> index(){
+        String response = "Data engineer test";
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
